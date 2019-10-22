97846d009eb30eb3c28f11b93b9c958454a025aa
Luca Brandalese <l.brandalese@campus.unimib.it>
2019-10-22 18:46:37 +0200

    aggiunta nuova stampa

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -2,6 +2,7 @@ package gitExercise2.main;
 
 public class HelloGit {
 	public static void main (String [] args) {
-		System.out.println("questa è una nuovissima stampa!!!");
+		System.out.println("hello git!!!");
+System.out.println("questa è una stampa!!!");
 	}
 }
