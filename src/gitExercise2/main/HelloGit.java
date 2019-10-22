939dd7b576e795f459be8409f1473095156269e9
Luca Brandalese <l.brandalese@campus.unimib.it>
2019-10-22 11:31:59 +0200

    sono il secondo development e ho  modificato una cazzo di rigagit add .git add .

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -2,6 +2,6 @@ package gitExercise2.main;
 
 public class HelloGit {
 	public static void main (String [] args) {
-		System.out.println("Hello git!!!");
+		System.out.println("questa è una nuova stampa!!!");
 	}
 }
