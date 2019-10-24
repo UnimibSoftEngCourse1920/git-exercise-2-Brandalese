0d710ad25a154c96417843bb2621721a7448dd1d
Luca Brandalese <l.brandalese@campus.unimib.it>
2019-10-24 14:39:32 +0200

    Sono il primo development e ho modificato una riga di codice

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -3,6 +3,6 @@ package gitExercise2.main;
 public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("hello git!!!");
-System.out.println("questa è una stampa!!!");
+System.out.println("questa è una nuovissima stampa, che serve per simulare la modifica simultanea di una riga di codice!");
 	}
 }
